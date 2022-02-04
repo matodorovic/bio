b72893fb43a91c57bb186c2868e6ff15c963a57a
Marta Anna Todorovic <martaannatodorovic@gmail,com>
2022-02-04 22:07:06 +0100

    Heroku test?

--- app.js
+++ app.js
@@ -113,6 +113,42 @@ app.use("/404", async (req, res) => {
 
 app.use("/", express.static("./public"));
 
+
+"use strict";
+
+const fs = require('fs');
+const path = require('path');
+const http = require('http');
+const staticBasePath = './';
+const PORT = process.env.PORT || 5080;
+
+const staticServe = function (req, res) {
+    let resolvedBase = path.resolve(staticBasePath);
+    let safeSuffix = path.normalize(req.url).replace(/^(\.\.[\/\\])+/, '');
+    let fileLoc = path.join(resolvedBase, safeSuffix);
+    fs.readFile(fileLoc, function (err, data) {
+        if (err) {
+            res.writeHead(404, 'Not Found');
+            res.write('404: File Not Found!');
+            return res.end();
+        }
+    res.statusCode = 200;
+    res.write(data);
+    return res.end();
+    });
+};
+
+const httpServer = http.createServer(staticServe);
+
+httpServer.listen(PORT, () => {
+    console.log("server listening on port " + PORT);
+});
+
+
+
+
+
 app.listen(5080);
 
 export default app;
+
