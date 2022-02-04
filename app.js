a76283017ebc254a169054a6b4aeeb502b5c96fa
Marta Anna Todorovic <martaannatodorovic@gmail,com>
2022-02-04 22:38:56 +0100

    Return to original state

--- app.js
+++ app.js
@@ -113,52 +113,7 @@ app.use("/404", async (req, res) => {
 
 app.use("/", express.static("./public"));
 
-
-var http = require('http').Server(app);
-
-// set up handlebars view engine
-var handlebars = require('express3-handlebars')
-  .create({ defaultLayout:'main' });
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
-
-http.listen(process.env.PORT || 5080, function(){
-  console.log('listening on', http.address().port);
-})
-
-// Home Page
-app.get('/', function(req, res){
-  res.render('home');
-});
-
-// About Page
-app.get('/about', function(req, res){
-  res.render('about');
-});
-
-// 404 catch-all handler (middleware)
-app.use(function(req, res){
-  res.status(404);
-  res.render('404');
-});
-
-// 500 error handler (middleware)
-app.use(function(req, res){
-  console.error(err.stack);
-  res.status(500);
-  res.render('500');
-});
-
-app.listen(app.get('port'), function(){
-console.log( 'Express started on http://localhost:' +
-app.get('port') + '; press Ctrl-C to terminate.' );
-});
-
-
-
-
-
-//app.listen(5080);
+app.listen(5080);
 
 export default app;
 
